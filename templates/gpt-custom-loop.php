237411906b39fdec3862e7c1782433e68954ef0e
Brad Potter <bradpotter@Brads-Mac-Pro.local>
2016-02-03 19:37:39 -0900

    Change loop. Rework taxonomy & terms.

--- templates/gpt-custom-loop.php
+++ templates/gpt-custom-loop.php
@@ -9,6 +9,7 @@
  * @link       http://bradpotter.com/plugins/genesis-page-templates
  */
 
+remove_action( 'genesis_loop', 'genesis_do_loop' );
 add_action( 'genesis_loop', 'genesis_page_templates_custom_loop' );
 /**
  * Genesis Page Templates Custom Loop.
@@ -17,34 +18,53 @@ add_action( 'genesis_loop', 'genesis_page_templates_custom_loop' );
  */
 function genesis_page_templates_custom_loop() {
 
+    global $paged;
+    global $query_args;
+    
 	$gcl_post_type = esc_attr( genesis_get_custom_field( '_gcl_post_type' ) );
-	$gcl_taxonomy = esc_attr( genesis_get_custom_field( '_gcl_taxonomy' ) );
-	$gcl_tax_term = esc_attr( genesis_get_custom_field( '_gcl_tax_term' ) );
-	$gcl_posts_per_page = esc_attr( genesis_get_custom_field( '_gcl_posts_per_page' ) );
-	$gcl_order_by = esc_attr( genesis_get_custom_field( '_gcl_order_by' ) );
-	$gcl_order = esc_attr( genesis_get_custom_field( '_gcl_order' ) );
-	$paged = get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1;
+    $gcl_taxonomy = esc_attr( genesis_get_custom_field( '_gcl_taxonomy' ) );
+    $gcl_tax_term = esc_attr( genesis_get_custom_field( '_gcl_tax_term' ) );
+    $gcl_posts_per_page = esc_attr( genesis_get_custom_field( '_gcl_posts_per_page' ) );
+    $gcl_order_by = esc_attr( genesis_get_custom_field( '_gcl_order_by' ) );
+    $gcl_order = esc_attr( genesis_get_custom_field( '_gcl_order' ) );
 	
-	$query_args = wp_parse_args(
-		array(
-			'post_type'        => $gcl_post_type,
-			'posts_per_page'   => $gcl_posts_per_page,
-			'orderby'          => $gcl_order_by,
-			'order'            => $gcl_order,
-			'paged'            => $paged,
-			)
-		);
+	$args = array(
+		'post_type'        => $gcl_post_type,
+		'posts_per_page'   => $gcl_posts_per_page,
+		'orderby'          => $gcl_order_by,
+		'order'            => $gcl_order,
+		'paged'            => $paged,
+	);
 	
-	if ( ! empty( $gcl_tax_term ) ) {
-		$terms = empty( $gcl_tax_term ) ? get_terms( $gcl_taxonomy ) : $gcl_tax_term;
+	if ( ! empty( $gcl_taxonomy ) && empty( $gcl_tax_term ) ) {
+		
+		$all_tax_terms = array();
+		$tax_terms = get_terms( $gcl_taxonomy );
+		
+		foreach ( $tax_terms as $tax_term ) {
+			
+			$all_tax_terms[] = $tax_term->slug;
+		}
+		
 		$args['tax_query'] = array(
 			array(
 				'taxonomy' => $gcl_taxonomy,
 				'field'    => 'slug',
-				'terms'    => $terms,
-				)
-			);
+				'terms'    => $all_tax_terms,
+			),
+		);
+	
+	} elseif ( ! empty( $gcl_taxonomy ) && ! empty( $gcl_tax_term ) ) {
+	
+		$args['tax_query'] = array(
+			array(
+				'taxonomy' => $gcl_taxonomy,
+				'field'    => 'slug',
+				'terms'    => $gcl_tax_term,
+			),
+		);
+	
 	}
 	
-	genesis_custom_loop( $query_args );
+	genesis_custom_loop( wp_parse_args( $query_args, $args ) );
 }
